01ea1b907a3388e7e264b5f28a465c3c199befc7
MARIA EDUARDA TESSARI <maria_tessari@estudante.sesisenai.org.br>
2026-09-01 09:29:48 -0300

    subindo novo projeto

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+export default function Card({ titulo, genero }){
+    return(
+        <div className="container">
+            <div>
+                <h1>Cine<strong>sTu</strong></h1>
+            </div>
+
+        </div>
+    )
+}
