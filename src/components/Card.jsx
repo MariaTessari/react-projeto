736070530143e2788befe4f6da89d5c6ef7bdf67
MARIA EDUARDA TESSARI <maria_tessari@estudante.sesisenai.org.br>
2026-09-04 07:43:25 -0300

    alterações

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,13 @@
-export default function Card({ titulo, genero }){
+export default function Header({ tema, toggleTema }){
     return(
         <div className="container">
             <div>
                 <h1>Cine<strong>sTu</strong></h1>
+
+                <button className="btn-theme" onClick={toggleTema}>
+
+                    {tema === "light" ? "dark" : "light"}
+                </button>
             </div>
 
         </div>
